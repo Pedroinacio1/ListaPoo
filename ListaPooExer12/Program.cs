4491de26fc762d0812e9e73444075c04e0be8518
Pedroinacio1 <166328383+Pedroinacio1@users.noreply.github.com>
2024-10-31 18:51:52 -0300

    Add files via upload

---
+++ ListaPooExer12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPooExer12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hipotenusa hp = new Hipotenusa();
+            Console.Write("Escreva o primeiro valor: ");
+            hp.setvl1(double.Parse(Console.ReadLine()));
+            Console.Write("Escreva o segundo valor: ");
+            hp.setvl2(double.Parse(Console.ReadLine()));
+            Console.Write("Escreva o terçeiro valor: ");
+            hp.setvl3(double.Parse(Console.ReadLine()));
+            hp.TrianguloRet();
+
+        }
+    }
+}
